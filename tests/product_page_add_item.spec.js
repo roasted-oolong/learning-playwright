d507260cc80031238b5e797f161a1d258cb5599d
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-03-03 20:31:31 +0000

    Learn about selectors and page interactions

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -2,5 +2,8 @@ import { test } from "@playwright/test"
 
 test("Product age Add To Basket", async ({ page }) => {
     await page.goto("localhost:2221")
-    await page.pause()
+    
+    const addToBasketButton = page.getByRole('button', { name: 'Add to Basket'}).first()
+    await addToBasketButton.waitFor()
+    await addToBasketButton.click()
 })
