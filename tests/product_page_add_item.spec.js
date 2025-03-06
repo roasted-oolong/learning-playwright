95527f69e4f780536ab52acaa6853aa25677e602
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-03-06 20:32:49 +0000

    Assert text changes

--- tests/product_page_add_item.spec.js
+++ tests/product_page_add_item.spec.js
@@ -1,9 +1,18 @@
-import { test } from "@playwright/test"
+import { test, expect } from "@playwright/test"
 
 test("Product age Add To Basket", async ({ page }) => {
     await page.goto("localhost:2221")
     
-    const addToBasketButton = page.getByRole('button', { name: 'Add to Basket'}).first()
+    const addToBasketButton = page.locator('[data-qa="product-button"]').first()
+    const basketCounter = page.locator('[data-qa="header-basket-count"]')
+
+    //Before action
     await addToBasketButton.waitFor()
+    await expect(addToBasketButton).toHaveText("Add to Basket")
+    await expect(basketCounter).toHaveText("0")
+
+    //After action
     await addToBasketButton.click()
+    await expect(addToBasketButton).toHaveText("Remove from Basket")
+    await expect(basketCounter).toHaveText("1")
 })
