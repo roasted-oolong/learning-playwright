049ac7a80604dde7664b59a75b08cd024366f136
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-03-10 18:06:09 +0000

    Accessing specific elements
    Learn how to add three products to the basket via page object.

--- page-objects/ProductPage.js
+++ page-objects/ProductPage.js
@@ -2,8 +2,14 @@
 export class ProductPage {
     constructor(page) {
         this.page = page
+        this.addButtons = page.locator('[data-qa="product-button"]')
     }
     visit = async () => {
         await this.page.goto("/")
+    }
+
+    addProductToBasket = async (index) => {
+        await this.addButtons.nth(index).waitFor()
+        await this.addButtons.nth(index).click()
     }
 }
--- tests/new_user_full_journey.spec.js
+++ tests/new_user_full_journey.spec.js
@@ -4,5 +4,8 @@ import { ProductPage } from "../page-objects/ProductPage"
 test.only("New user full end-to-end test journey", async ({ page }) => {
     const productPage = new ProductPage(page)
     await productPage.visit()
+    await productPage.addProductToBasket(0)
+    await productPage.addProductToBasket(1)
+    await productPage.addProductToBasket(2)
     await page.pause()
 })
